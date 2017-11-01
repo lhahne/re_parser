502579c73af2f3148f5866dcf10dd228cbd2697e
Lauri Hahne <lauri.hahne@gmail.com>
2017-11-01 20:05:12 +0200

    pdf download

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const mailparser = require('mailparser');
 const cheerio = require('cheerio');
 const request = require('request');
 
+const aws = require('aws-sdk');
+const s3Stream = require('s3-upload-stream')(new aws.S3());
+
 exports.handler = (event, context, callback) => {
     // TODO implement
     const message = JSON.parse(event.Records[0].Sns.Message);
@@ -10,12 +13,26 @@ exports.handler = (event, context, callback) => {
     mailparser.simpleParser(message.content, (err, mail) => {
         const mailHtml = cheerio.load(mail.html);
         const url = mailHtml('tbody > tr:nth-child(5) > td:nth-child(2) > table > tbody > tr:nth-child(1) > td > a').attr('href');
-        console.log(url);
-        
-        request(url.split("?")[0], (error, response, body) => {
-            console.log(body);
-            callback(null, 'Hello from Lambda');
+        const id = url.split("?")[0].split("/").pop();
+        const pdfUrl = "https://www.etuovi.com/kohde_pdf/" + id + "?download=pdf";
+        console.log(pdfUrl);
+
+        const upload = s3Stream.upload({
+            Bucket: "etuovi.hahne.fi",
+            Key: "pdf/" + id + ".pdf"
         });
+
+        upload.on('error', (error) => {
+            console.log(error);
+            callback(null, 'errror');
+        });
+
+        upload.on('uploaded', (details) => {
+            console.log(details);
+            callback(null, 'finished');
+        });
+        
+        request(pdfUrl).pipe(upload);
     });
     
 };
